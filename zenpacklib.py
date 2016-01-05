5d71cf3a45ad5432064fba83eecaa6582ab2bad9
Chet Luther <chet.luther@gmail.com>
2016-01-05 15:22:44 -0500

    Bump versions for 1.0.5 release.

--- zenpacklib.py
+++ zenpacklib.py
@@ -27,7 +27,7 @@ This module provides a single integration point for common ZenPacks.
 """
 
 # PEP-396 version. (https://www.python.org/dev/peps/pep-0396/)
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 
 import logging
