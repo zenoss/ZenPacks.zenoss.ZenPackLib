542880e169b1ed605c0d5bb719ef4a739f376681
Joseph Anderson <j053ph4j@gmail.com>
2016-07-19 08:38:09 -0500

    added Zenoss platform version detection

--- zenpacklib.py
+++ zenpacklib.py
@@ -2553,8 +2553,10 @@ class ClassSpec(Spec):
                     if cached:
                         r = self.cacheRRDValue(datapoint, default=default)
                     else:
-                        r = self.getRRDValue(datapoint, start=time.time()-1800)
-
+                        if is_zenoss_4():
+                            r = self.getRRDValue(datapoint, start=time.time()-1800)
+                        else:
+                            r = self.getRRDValue(datapoint, start=time.time()-1800)
                     if r is not None:
                         if not math.isnan(float(r)):
                             return r
@@ -5722,6 +5724,14 @@ OrderAndValue = collections.namedtuple('OrderAndValue', ['order', 'value'])
 
 
 # Private Functions #########################################################
+def is_zenoss_4():
+    '''return True if Zenoss is version 4.x or below'''
+    from Products.ZenModel.ZVersion import VERSION
+    from Products.ZenUtils.Version import Version
+    if Version.parse('Zenoss %s' % VERSION) < Version.parse('Zenoss 5'):
+        return True
+    return False
+
 
 def get_zenpack_path(zenpack_name):
     """Return filesystem path for given ZenPack."""
