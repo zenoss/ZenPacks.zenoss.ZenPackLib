e5b5fc2a65ed8bbbf6b3a0f11dbc51be38b7bbce
Joseph Anderson <j053ph4j@gmail.com>
2016-10-10 14:27:02 -0500

    Log symlink create/remove during install
    - added log messages for zenpack install methods
    - updated copyright header boilerplate

--- ZenPacks/zenoss/ZenPackLib/__init__.py
+++ ZenPacks/zenoss/ZenPackLib/__init__.py
@@ -1,19 +1,16 @@
-# Nothing is required in this __init__.py, but it is an excellent place to do
-# many things in a ZenPack.
+##############################################################################
 #
-# The example below which is commented out by default creates a custom subclass
-# of the ZenPack class. This allows you to define custom installation and
-# removal routines for your ZenPack. If you don't need this kind of flexibility
-# you should leave the section commented out and let the standard ZenPack
-# class be used.
+# Copyright (C) Zenoss, Inc. 2016, all rights reserved.
 #
-# Code included in the global scope of this file will be executed at startup
-# in any Zope client. This includes Zope itself (the web interface) and zenhub.
-# This makes this the perfect place to alter lower-level stock behavior
-# through monkey-patching.
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
 import os
 import Globals
 import sys
+import logging
+LOG = logging.getLogger('zen.ZenPackLib')
 
 from Products.ZenModel.ZenPack import ZenPack as ZenPackBase
 from Products.ZenUtils.Utils import unused, zenPath
@@ -35,7 +32,6 @@ class ZenPack(ZenPackBase):
 
     def remove(self, dmd, leaveObjects=False):
         if not leaveObjects:
-
             pass
         self.remove_symlink()
         ZenPackBase.remove(self, dmd, leaveObjects=leaveObjects)
@@ -45,12 +41,14 @@ class ZenPack(ZenPackBase):
         shell_path = os.path.join(basedir, "bin/zenpacklib")
         py_path = os.path.join(basedir, 'zenpacklib.py')
         for x in [shell_path, py_path]:
+            LOG.info('Setting permissions on {}'.format(x))
             os.chmod(x, 0755)
 
     def create_symlink(self):
         '''create symlink'''
         source_path = os.path.join(os.path.dirname(__file__), "bin/zenpacklib")
         dest_path = zenPath('bin', 'zenpacklib')
+        LOG.info('Creating symlink {}'.format(dest_path))
         os.system('ln -sf "%s" "%s"' % (source_path, dest_path))
         os.system('chmod 0755 %s' % dest_path)
 
@@ -58,3 +56,4 @@ class ZenPack(ZenPackBase):
         '''remove symlink'''
         dest_path = zenPath('bin', 'zenpacklib')
         os.system('rm -f "%s"' % dest_path)
+        LOG.info('Removing symlink {}'.format(dest_path))
