6c68c000adcd75944ac6dc6749db0a7f07a5bf40
Joseph Anderson <j053ph4j@gmail.com>
2016-11-04 09:31:06 -0500

    Add proxies for all platform component classes
    ZenPacks have been created that wanted to extend platform component
    classes such as IpInterface and FileSystem. The mechanism they used to
    do this was to set the base classes to zenpacklib.Component first, and
    something like Products.ZenModel.IpInterface.IpInterface second. This
    resulted in buggy behavior because IpInterface behavior such as its
    index_object() method would never be called.
    
    Introducing zenpacklib proxies for all platform component classes solves
    this problem because it puts a zenpacklib class as the single parent
    class. This allows zenpacklib to properly fan out calls to things like
    index_object() to all necessary parent classes.
    
    Refs ZEN-25559.

--- ZenPacks/zenoss/ZenPackLib/lib/base/Component.py
+++ ZenPacks/zenoss/ZenPackLib/lib/base/Component.py
@@ -6,9 +6,46 @@
 # License.zenoss under the directory where your Zenoss product is installed.
 #
 ##############################################################################
-from Products.ZenModel.DeviceComponent import DeviceComponent
-from Products.ZenModel.ManagedEntity import ManagedEntity
+from Products.ZenModel.CPU import CPU as BaseCPU
+from Products.ZenModel.DeviceComponent import DeviceComponent as BaseDeviceComponent
+from Products.ZenModel.ExpansionCard import ExpansionCard as BaseExpansionCard
+from Products.ZenModel.Fan import Fan as BaseFan
+from Products.ZenModel.FileSystem import FileSystem as BaseFileSystem
+from Products.ZenModel.HardDisk import HardDisk as BaseHardDisk
+from Products.ZenModel.HWComponent import HWComponent as BaseHWComponent
+from Products.ZenModel.IpInterface import IpInterface as BaseIpInterface
+from Products.ZenModel.IpRouteEntry import IpRouteEntry as BaseIpRouteEntry
+from Products.ZenModel.IpService import IpService as BaseIpService
+from Products.ZenModel.ManagedEntity import ManagedEntity as BaseManagedEntity
+from Products.ZenModel.OSComponent import OSComponent as BaseOSComponent
+from Products.ZenModel.OSProcess import OSProcess as BaseOSProcess
+from Products.ZenModel.PowerSupply import PowerSupply as BasePowerSupply
+from Products.ZenModel.Service import Service as BaseService
+from Products.ZenModel.TemperatureSensor import TemperatureSensor as BaseTemperatureSensor
+from Products.ZenModel.WinService import WinService as BaseWinService
+
 from ..factory.ComponentTypeFactory import ComponentTypeFactory
 
 
-Component = ComponentTypeFactory('Component', (DeviceComponent, ManagedEntity))
+Component = ComponentTypeFactory('Component', (BaseDeviceComponent, BaseManagedEntity))
+HWComponent = ComponentTypeFactory('HWComponent', (BaseHWComponent,))
+
+CPU = ComponentTypeFactory('CPU', (BaseCPU,))
+ExpansionCard = ComponentTypeFactory('ExpansionCard', (BaseExpansionCard,))
+Fan = ComponentTypeFactory('Fan', (BaseFan,))
+HardDisk = ComponentTypeFactory('HardDisk', (BaseHardDisk,))
+PowerSupply = ComponentTypeFactory('PowerSupply', (BasePowerSupply,))
+TemperatureSensor = ComponentTypeFactory('TemperatureSensor', (BaseTemperatureSensor,))
+
+OSComponent = ComponentTypeFactory('OSComponent', (BaseOSComponent,))
+FileSystem = ComponentTypeFactory('FileSystem', (BaseFileSystem,))
+IpInterface = ComponentTypeFactory('IpInterface', (BaseIpInterface,))
+IpRouteEntry = ComponentTypeFactory('IpRouteEntry', (BaseIpRouteEntry,))
+OSProcess = ComponentTypeFactory('OSProcess', (BaseOSProcess,))
+
+Service = ComponentTypeFactory('Service', (BaseService,))
+IpService = ComponentTypeFactory('IpService', (BaseIpService,))
+WinService = ComponentTypeFactory('WinService', (BaseWinService,))
+
+# Backwards-compatibility aliases.
+HardwareComponent = HWComponent
