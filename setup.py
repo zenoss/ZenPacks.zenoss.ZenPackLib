3280b1dc85e8edf1a5b9fd8f7cc416728ab952f9
Joseph Anderson <j053ph4j@gmail.com>
2017-01-11 11:12:51 -0600

    Start 2.0.1 Hotfix: Version 2.0.0 -> 2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.ZenPackLib"
-VERSION = "2.0.0"
+VERSION = "2.0.1"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
