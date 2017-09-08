08bb823a1abafd7b73c9b81d21be70b40b0450d3
Philip Carinhas <pcarinhas@zenoss.com>
2017-09-08 15:15:34 -0500

    Update version

--- ZenPacks/zenoss/ZenPackLib/zenpacklib.py
+++ ZenPacks/zenoss/ZenPackLib/zenpacklib.py
@@ -27,7 +27,7 @@ This module provides a single integration point for common ZenPacks.
 """
 
 # PEP-396 version. (https://www.python.org/dev/peps/pep-0396/)
-__version__ = "2.0.7"
+__version__ = "2.0.8"
 
 # Must defer definition of TestCase. Otherwise it imports
 # BaseTestCase which puts Zope into testing mode.
