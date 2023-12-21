6adedc47e9ebe2e52c420c53a3922fb912553510
Steve Paras-Charlton <stevepc@zenoss.com>
2023-12-20 18:48:51 -0700

    ZPS-8777: ZEN-34366: migrate script for tempalte add/replace

--- ZenPacks/zenoss/ZenPackLib/migrate/FixReplacementTemplates.py
+++ ZenPacks/zenoss/ZenPackLib/migrate/FixReplacementTemplates.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (C) Zenoss, Inc. 2019, all rights reserved.
+# Copyright (C) Zenoss, Inc. 2023, all rights reserved.
 #
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
@@ -28,26 +28,26 @@ class FixReplacementTemplates(ZenPackMigration):
                 objchanged = False
                 availtempls = None
                 templs = obj.zDeviceTemplates
-                for templ in templs:
+                for templ in list(templs):
                     if templ.endswith('-replacement') or templ.endswith('-addition'):
                         if availtempls = None:
-                            availtempls = [t.id for t in obj.getAvailableTemplates]
+                            availtempls = [t.id for t in obj.getAvailableTemplates()]
                         base = templ.rsplit('-',1)[0]
                         if base in availtempls:
-                            LOG.info("Removing template %s from %s.zDeviceTemplates", templ, dc.id)
+                            LOG.info("Removing template %s from %s.zDeviceTemplates", templ, obj.id)
                             templs.remove(templ)
                             if base not in templs:
-                                LOG.info("Adding base template %s to %s.zDeviceTemplates", base, dc.id)
+                                LOG.info("Adding base template %s to %s.zDeviceTemplates", base, obj.id)
                                 templs.append(base)
                         else:
-                            LOG.warning("base template %s for template %s was not found, not migrating.")
+                            LOG.warning("base template %s for template %s was not found, not migrating.", base, templ)
                             continue
                         objchanged = True
                 if objchanged:
                     obj.setZenProperty('zCollectorPlugins', templs)
 
     def migrate(self, pack):
-        LOG.info("fix zDeviceTempaltes on /Devices and subclasses")
+        LOG.info("fix zDeviceTemplates on /Devices and subclasses")
 
         )
         deviceClass = pack.dmd.Devices
